cf9ed4a2e116ac34c3c32ffc77ee959163deefb7
Albert Casals <skarbat@gmail.com>
2014-03-07 08:24:01 -0500

    Removing kdesk build and packaging, it's being moved to "kdesk" repo

--- src/main.h
+++
@@ -1,7 +0,0 @@
-//
-// main.h
-//
-
-#define FILE_KDESKRC       "/usr/share/kano-desktop/idesk/ideskrc"
-#define DIR_KDESKTOP       "/usr/share/kano-desktop/idesk/idesktop"
-#define DIR_KDESKTOP_USER  ".kdestop"
