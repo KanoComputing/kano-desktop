0de36028775e9f8d7bf661b6f1d372e584c80994
Alex Simon <alejandro@kano.me>
2015-02-09 17:52:42 +0000

    Fixed typo in comment

--- bin/open-me.sh
+++ bin/open-me.sh
@@ -4,7 +4,7 @@
 # Copyright (C) 2014 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
 #
-# Execute this file to receive the File Finder batdge
+# Execute this file to receive the File Finder badge
 #
 
 clear
